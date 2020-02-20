22369757beb01488de82db4350cde0ba768546d0
Haikhell <ivanhaikhell@gmail.com>
2020-02-20 18:19:16 +0300

    s

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const dbHelper = require('./db');
-const config = require('./config');
+const dbHelper = require('./src/db');
+const config = require('./src/config');
 
 (async () => {
   await dbHelper.mongodbClient.connect();
@@ -9,7 +9,7 @@ const config = require('./config');
   dbHelper.mongodbClient.on('error', (err) => logger.error(err));
   dbHelper.mongodbClient.on('close', () => logger.error('Mongo driver connection disconnected'));
 
-  const app = require('./app');
+  const app = require('./src/app');
 
   const httpServer = http.createServer(app);
 
